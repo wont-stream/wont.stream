01c6736b830f25d58adee051d02ee65d4d632416
Unstream <git@wont.stream>
2024-03-16 23:35:42 -0400

    oops

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { $, component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+import { $, component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 import { LuCast, LuGithub } from "@qwikest/icons/lucide";
