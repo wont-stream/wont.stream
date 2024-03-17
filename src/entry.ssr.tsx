a5cddd9b8eed671e3755c3ea15dd169974ae1cfd
Unstream <git@wont.stream>
2024-03-17 15:52:41 -0400

    fix

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -24,7 +24,7 @@ export default function (opts: RenderToStreamOptions) {
     // Use container attributes to set attributes on the html tag.
     containerAttributes: {
       lang: "en-us",
-      base: "https://cdn.jsdelivr.net/gh/wont-stream/wont-stream.github.io@main/builds/",
+      //base: "https://cdn.jsdelivr.net/gh/wont-stream/wont-stream.github.io@main/builds/",
       ...opts.containerAttributes,
     },
   });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,9 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 import { LuCast, LuGithub } from "@qwikest/icons/lucide";
 
 export default component$(() => {
-  const pageSize = useSignal({width: 0, height:0})
-  const imgStyle = useSignal({opacity: "0", animation: ""})
+  const pageSize = useSignal({ width: 0, height: 0 })
+  const imgStyle = useSignal({ opacity: "0", animation: "" })
+  const imgURL = useSignal("")
   const imgLoad = $(() => {
     imgStyle.value = {
       animation: "fadeIn 1s",
@@ -15,16 +16,17 @@ export default component$(() => {
 
   useOnWindow("load", $(() => {
     pageSize.value.width = window.innerWidth
-  pageSize.value.height = window.innerHeight
-}))
+    pageSize.value.height = window.innerHeight
+    imgURL.value = `https://img.wont.stream/bg.avif?w=${window.innerWidth}&h=${window.innerHeight}`
+  }))
 
   return (
     <>
-      <img src={`https://img.wont.stream/bg.avif?w=${pageSize.value.width}&h=${pageSize.value.height}`} decoding="async" loading="lazy" id="bg" onLoad$={imgLoad} onError$={imgLoad} style={imgStyle.value} width={pageSize.value.width} height={pageSize.value.height}></img>
+      <img src={imgURL.value} decoding="async" loading="lazy" id="bg" onLoad$={imgLoad} onError$={imgLoad} style={imgStyle.value} width={pageSize.value.width} height={pageSize.value.height}></img>
       <div class="center" style={imgStyle.value}>
-      <h1><LuCast style={{ width: "100", height: "100" }} /><br />Unstream</h1>
-      <p>A self taught fullstack developer.</p>
-      <p><a href="https://github.com/wont-stream" aria-label="GitHub"><LuGithub /></a></p>
+        <h1><LuCast style={{ width: "100", height: "100" }} /><br />Unstream</h1>
+        <p>A self taught fullstack developer.</p>
+        <p><a href="https://github.com/wont-stream" aria-label="GitHub"><LuGithub /></a></p>
       </div>
     </>
   );
